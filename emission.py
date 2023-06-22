79c0458a618873928add9b6af4fffac5f20d3e8a
Henry <henry.leighton97@gmail.com>
2023-06-22 23:57:14 +1000

    Initial changes for the big merge

--- emission.py
+++ emission.py
@@ -29,15 +29,21 @@ transport = db['transport']
 stationary_energy = db['stationary_energy']
 geo_data = db['geo_data']
 combined = db['combined']
+geo_data_lga = db['geo_data_lga']
 
 app = Flask(__name__)
 
 # Landing Page
 @app.route("/")
 def index():
-    """List all available api routes."""
     return render_template("index.html")
 
+# Bar Chart
+@app.route("/bar_chart")
+def barchart():
+    return render_template("bar_chart.html")
+
+# Extract query results & josnification
 def find_all(query_result):
     temp_list = []
     for row in query_result:
@@ -45,60 +51,134 @@ def find_all(query_result):
     json_variables = json.dumps(temp_list,default=json_util.default)
     return json_variables 
 
-
-@app.route("/get_stationary_energy",methods=['GET'])
-def get_stationary_energy():
-    results = stationary_energy.find({})
-    return find_all(results)
-
-@app.route("/get_geo_data",methods=['GET'])
-def get_geo_data():
-    # Fetch stationary energy documents
+# Build geodata for each sector
+def buildGeodata(sec_data):
+    unset_emmision_value = {'$set':{'properties.emission_amt':0}}
+    geo_data.update_many({},unset_emmision_value)
+    for i in range(len(sec_data)):
+        query = {'id':sec_data[i]['_id']['LGA']}
+        new_prop = {'$set':{'properties.emission_amt':sec_data[i]['emission_amt']}}
+        geo_data.update_one(query,new_prop)
     results = geo_data.find({})
-    return find_all(results)
+    return results
 
-@app.route("/get_transport",methods=['GET'])
-def get_transport():
-    # Fetch transport documents
-    results = transport.find({})
-    return find_all(results)
-
-@app.route("/get_waste",methods=['GET'])
-def get_waste():
-    # Fetch waste documents
-    results = waste.find({})
-    return find_all(results)
-
-@app.route("/get_electricity_generation",methods=['GET'])
-def get_electricity_generation():
-    # Fetch electricity generation documents
-    results = electricity_generation.find({})
-    return find_all(results)
-
-@app.route("/get_agriculture",methods=['GET'])
-def get_agriculture():
-    # Fetch agriculture  documents
-    results = agriculture.find({})
-    return find_all(results)
-
-
-@app.route("/get_lulucf",methods=['GET'])
-def get_lulucf():
-    # Fetch lulucf  documents
-    results = lulucf.find({})
-    return find_all(results)
-
-@app.route("/get_elec_consumption",methods=['GET'])
-def get_elec_consumption():
- # Fetch electricity consumption docuemnts
-    results = electricity_consumption.find({})
-    return find_all(results)
+agri ='Agriculture'
+elec_cons ='Electricity Consumption - Scope 2'
+fugi ='Fugitive Emissions'
+land_use ='Land Use, Land Use Change and Forestry'
+elec_gen ='Electricity Generation'
+wte ='Waste'
+trans ='Transport'
+stat_eng ='Stationary Energy (excluding Electricity Generation)'
 
 @app.route("/get_all_data",methods=['GET'])
 def get_all_data():
     # Fetch fugitive  documents
     results = combined.find({})
     return find_all(results)
+
+# Build doughnut chart
+
+@app.route("/get_by_lga_year/<lga>/<year>",methods=['GET'])
+def get_by_lga_year(lga,year):
+    lga = str(lga).strip()
+    year = int(year)
+    sector_data =list(combined.aggregate([
+
+            {'$match':{ '$and':[ {'Year':year},{'LGA':lga}] }},
+            {'$group': {'_id': {"Sector": "$Sector"},'emission_amt':{'$sum':"$Emissions_tonnes_CO2-e"}}},
+            {'$sort':{'emission_amt':-1}}
+            ]))
+    # print([sector_data])
+    return (find_all(sector_data))
+    
+
+# Build Henry's chart
+# @app.route("/get_all_count/<year>",methods=['GET'])
+# def get_by_lga_year(year):
+#     year = int(year)
+#     sector_data =list(combined.aggregate([
+
+#             {'$match':{ '$and':[ {'Year':year}] }},
+#             {'$group': {'_id': {"Sector": "$Sector"},'emission_amt':{'$sum':"$Emissions_tonnes_CO2-e"}}},
+#             {'$sort':{'emission_amt':-1}}
+#             ]))
+#     # print([sector_data])
+#     return (find_all(sector_data))
+    
+# Code to build Choroplath
+
+# @app.route("/get_geo_data/<sector>/<year>",methods=['GET']) # Data set given by keerthi
+# def get_geo_data(sector,year):
+#     # agri = agri.strip()
+#     year = year.strip()
+#     sector = str(sector).strip()
+   
+#     #Agriculture
+#     if str(sector) == str(agri): 
+    
+#         sector_data =list(agriculture.aggregate([
+#             {'$match':{ '$and':[ {'Year':year},{'Sector':sector}] }},
+#             {'$group': {'_id': {"LGA": "$LGA_CODE18"},'emission_amt':{'$sum':"$Emissions"}}},
+#             {'$sort':{'emission_amt':-1}}
+#             ]))
+#         geo_pack = buildGeodata(sector_data)
+#         return find_all(geo_pack)
+    
+#     # # Electric Consumptions
+#     if (sector == elec_cons): 
+#         sector_data =list(electricity_consumption.aggregate([
+#             {'$match':{ '$and':[ {'Year':year},{'Sector':sector}] }},
+#             {'$group': {'_id': {"LGA": "$LGA_CODE18"},'emission_amt':{'$sum':"$Emissions"}}},
+#             {'$sort':{'emission_amt':-1}}
+#             ]))
+        
+#         geo_pack = buildGeodata(sector_data)
+#         return find_all(geo_pack)
+   
+ 
+
+# Code to build stacked area chart
+@app.route("/get_all_year/<lga>",methods=['GET'])
+def get_all_year(lga):
+    years = [2016,2017,2018,2019]
+    sectors = [agri,elec_cons,fugi,land_use,elec_gen,wte,trans,stat_eng]
+   
+    year_by_data = []
+    for y in range(len(years)):
+        sector_by_data = []
+        for i in range(len(sectors)):
+            templist = []
+            query = {'Year':int(years[y]),'LGA':lga,'Sector':sectors[i]}
+            fields = {'Emissions_tonnes_CO2-e':1,'LGA':1,'LGA_CODE18':1,'Sector':1,'SubSector1':1,'Year':1}
+            results = combined.find(query,fields)
+            for row in results:
+                templist.append(row)
+            sector_by_data.append(templist)
+        year_by_data.append(sector_by_data)
+    json_variables = json.dumps(year_by_data,default=json_util.default)
+    return json_variables
+
+   
+
+# @app.route("/get_by_year/<year>/<lga>",methods=['GET'])
+# def get_by_year(year,lga):
+#     sectors = ['Electricity Consumption - Scope 2','Agriculture',
+#                 'Fugitive Emissions','Land Use, Land Use Change and Forestry',
+#                 'Electricity Generation','Waste','Transport',
+#                 'Stationary Energy (excluding Electricity Generation)']
+#     sector_by_data = []
+#     for i in range(len(sectors)):
+#         templist = []
+#         query = {'Year':int(year),'LGA':lga,'Sector':sectors[i]}
+#         results = combined.find(query)
+#         for row in results:
+#             templist.append(row)
+#         sector_by_data.append(templist)
+#     json_variables = json.dumps(sector_by_data,default=json_util.default)
+#     return json_variables
+
+
 
 @app.route("/get_by_year/<year>/<lga>",methods=['GET'])
 def get_by_year(year,lga):
@@ -118,6 +198,128 @@ def get_by_year(year,lga):
     json_variables = json.dumps(sector_by_data,default=json_util.default)
     return json_variables
 
+## KKC - added ##
+@app.route("/getYears",methods=['GET'])
+def get_Years():
+ # Fetch the list of years from the database
+    results = combined.distinct('Year')
+    return find_all(results)
+
+@app.route("/getLGAs",methods=['GET'])
+def get_LGAs():
+ # Fetch the list of years from the database
+    results = combined.distinct('LGA')
+    return find_all(results)
+
+@app.route("/getSectors",methods=['GET'])
+def get_Sectors():
+ # Fetch the list of years from the database
+    results = combined.distinct('Sector')
+    return find_all(results)
+
+@app.route("/get_Year_data/<LGA>/<Sector>",methods=['GET'])
+def get_Year_data(LGA, Sector):
+ # Fetch the date for this LGA/Sector combination
+    if LGA == 'All':
+        if Sector == 'All':
+            aggs = [
+                    { "$group": { "_id": "$Year", "totalEmission": { "$sum": "$Emissions_tonnes_CO2-e" }}
+                    }
+                   ]
+        else:
+            aggs = [
+                    { "$match":{ "Sector": Sector }
+                    },
+                    { "$group": { "_id": "$Year", "totalEmission": { "$sum": "$Emissions_tonnes_CO2-e" }}
+                    }
+                   ]
+    elif Sector == 'All':
+        aggs = [
+                { "$match":{ "LGA": LGA }
+                },
+                { "$group": { "_id": "$Year", "totalEmission": { "$sum": "$Emissions_tonnes_CO2-e" }}
+                }
+               ]
+    else:
+        aggs = [
+                { "$match":{ "LGA": LGA, "Sector": Sector }
+                },
+                { "$group": { "_id": "$Year", "totalEmission": { "$sum": "$Emissions_tonnes_CO2-e" }}
+                }
+               ]
+    print(aggs)
+    results = combined.aggregate(aggs)
+    return find_all(results)
+
+@app.route("/get_LGA_data/<Year>/<Sector>",methods=['GET'])
+def get_LGA_data(Year, Sector):
+ # Fetch the date for this Year/Sector combination
+    if Year == 'All':
+        if Sector == 'All':
+            aggs = [
+                    { "$group": { "_id": "$LGA", "totalEmission": { "$sum": "$Emissions_tonnes_CO2-e" }}
+                    }
+                   ]
+        else:
+            aggs = [
+                    { "$match":{ "Sector": Sector }
+                    },
+                    { "$group": { "_id": "$LGA", "totalEmission": { "$sum": "$Emissions_tonnes_CO2-e" }}
+                    }
+                   ]
+    elif Sector == 'All':
+            aggs = [
+                    { "$match":{ "Year": int(Year) }
+                    },
+                    { "$group": { "_id": "$LGA", "totalEmission": { "$sum": "$Emissions_tonnes_CO2-e" }}
+                    }
+                   ]
+    else:
+        aggs = [
+                { "$match":{ "Year": int(Year), "Sector": Sector }
+                },
+                { "$group": { "_id": "$LGA", "totalEmission": { "$sum": "$Emissions_tonnes_CO2-e" }}
+                }
+               ]
+    print(aggs)
+    results = combined.aggregate(aggs)
+    return find_all(results)
+
+@app.route("/get_Sector_data/<Year>/<LGA>",methods=['GET'])
+def get_Sector_data(Year, LGA):
+ # Fetch the date for this Year/LGA combination
+    if Year == 'All':
+        if LGA == 'All':
+            aggs = [
+                    { "$group": { "_id": "$Sector",  "totalEmission": { "$sum": "$Emissions_tonnes_CO2-e" }}
+                    }
+                   ]
+        else:
+            aggs = [
+                    { "$match":{ "LGA": LGA }
+                    },
+                    { "$group": { "_id": "$Sector",  "totalEmission": { "$sum": "$Emissions_tonnes_CO2-e" }}
+                    }
+                   ]
+    elif LGA == 'All':
+        aggs = [
+                { "$match":{ "Year": int(Year) }
+                },
+                { "$group": { "_id": "$Sector",  "totalEmission": { "$sum": "$Emissions_tonnes_CO2-e" }}
+                }
+               ]
+    else:
+        aggs = [
+                { "$match":{ "Year": int(Year), "LGA": LGA }
+                },
+                { "$group": { "_id": "$Sector",  "totalEmission": { "$sum": "$Emissions_tonnes_CO2-e" }}
+                }
+               ]
+    print(aggs)
+    results = combined.aggregate(aggs)
+    return find_all(results)
+
+## KKC - added end ##​
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -18,102 +18,199 @@ function closeSidebar() {
 }
 
 
-
-// ---------- CHARTS ----------
-
-// BAR CHART
-var barChartOptions = {
-  series: [{
-    data: [10, 8, 6, 4, 2]
-  }],
+// RADIAL GAUGE
+var radialChartOptions = {
+  series: [3.03, 26.67],
   chart: {
-    type: 'bar',
-    height: 350,
-    toolbar: {
-      show: false
-    },
-  },
-  colors: [
-    "#246dec",
-    "#cc3c43",
-    "#367952",
-    "#f5b74f",
-    "#4f35a1"
-  ],
-  plotOptions: {
-    bar: {
-      distributed: true,
-      borderRadius: 4,
-      horizontal: false,
-      columnWidth: '40%',
-    }
-  },
-  dataLabels: {
-    enabled: false
-  },
-  legend: {
-    show: false
-  },
-  xaxis: {
-    categories: ["Laptop", "Phone", "Monitor", "Headphones", "Camera"],
-  },
-  yaxis: {
-    title: {
-      text: "Count"
-    }
+  height: 200,
+  type: 'radialBar',
+  toolbar: {
+    show: true
   }
-};
-
-var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
-barChart.render();
-
-
-
-// AREA CHART
-var areaChartOptions = {
-  series: [{
-    name: 'Purchase Orders',
-    data: [31, 40, 28, 51, 42, 109, 100]
-  }, {
-    name: 'Sales Orders',
-    data: [11, 32, 45, 32, 34, 52, 41]
-  }],
-  chart: {
-    height: 350,
-    type: 'area',
-    toolbar: {
-      show: false,
-    },
   },
-  colors: ["#4f35a1", "#246dec"],
-  dataLabels: {
-    enabled: false,
+  plotOptions: {
+    radialBar: {
+      startAngle: -135,
+      endAngle: 225,
+        hollow: {
+        margin: 0,
+        size: '70%',
+        background: '#fff',
+        image: undefined,
+        imageOffsetX: 0,
+        imageOffsetY: 0,
+        position: 'front',
+        dropShadow: {
+          enabled: true,
+          top: 3,
+          left: 0,
+          blur: 4,
+          opacity: 0.24
+        }
+      },
+      track: {
+        background: '#fff',
+        strokeWidth: '67%',
+        margin: 0, // margin is in pixels
+        dropShadow: {
+          enabled: true,
+          top: -3,
+          left: 0,
+          blur: 4,
+          opacity: 0.35
+        }
+      },
+      dataLabels: {
+        show: true,
+        name: {
+          offsetY: -10,
+          show: true,
+          color: '#888',
+          fontSize: '14px'
+        },
+        value: {
+          formatter: function(val) {
+            return parseInt(val);
+          },
+          color: '#111',
+          fontSize: '36px',
+          show: true,
+        }
+      }
+    }
+  },
+  fill: {
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      type: 'horizontal',
+      shadeIntensity: 0.5,
+      gradientToColors: ['#ABE5A1'],
+      inverseColors: true,
+      opacityFrom: 1,
+      opacityTo: 1,
+      stops: [0, 100]
+    }
   },
   stroke: {
-    curve: 'smooth'
+    lineCap: 'round'
   },
-  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
-  markers: {
-    size: 0
-  },
-  yaxis: [
-    {
-      title: {
-        text: 'Purchase Orders',
-      },
-    },
-    {
-      opposite: true,
-      title: {
-        text: 'Sales Orders',
-      },
-    },
-  ],
-  tooltip: {
-    shared: true,
-    intersect: false,
-  }
+  labels: ['Actual Percent', 'Target Percent'],
 };
+var radialChart = new ApexCharts(document.querySelector("#radial-chart"), radialChartOptions);
+radialChart.render();
 
-var areaChart = new ApexCharts(document.querySelector("#area-chart"), areaChartOptions);
-areaChart.render();
+// // KKC - added to set the options in the dropdown lists for Year, LGA and Emission Source
+// var Year = "All";
+// var LGA = "All";
+// var Sector = "All";
+// var barData = []
+// var maxBarX = 0;
+// var barX;
+// var barY;
+// var barWidth = 800;
+// var barHeight = 600;
+// var barWidth2 = 1200;
+// var barHeight2 = 600;
+// var barMargin = {top: 50, bottom: 100, left: 50, right: 50}
+
+// d3.select("#Years").selectAll("*").remove();
+// d3.json("getYears").then(function(data) {
+//     YearOptions = d3.select("#Years");
+//     YearOptions.append('option').text('All');
+//     for (i = 0; i< data.length; i++) {
+//         if (Year == "") {
+//             console.log(data[i])
+//             Year = data[i];
+//             YearOptions.append('option')
+//             .attr('selected', '').text(data[i]);
+//         } 
+//         else if (Year == data[i]) {
+//             YearOptions.append('option')
+//             .attr('selected', '')
+//             .text(data[i]);
+//         } 
+//         else {
+//             YearOptions.append('option')
+//             .text(data[i]);
+//         }
+//     }
+// });
+
+// d3.select("#LGAs").selectAll("*").remove();
+// d3.json("getLGAs").then(function(data) {
+//     LGAOptions = d3.select("#LGAs");
+//     LGAOptions.append('option').text('All');
+//     for (i = 0; i< data.length; i++) {
+//         if (LGA == "") {
+//             console.log(data[i])
+//             LGA = data[i];
+//             LGAOptions.append('option')
+//             .attr('selected', '').text(data[i]);
+//         } 
+//         else if (LGA == data[i]) {
+//             LGAOptions.append('option')
+//             .attr('selected', '').text(data[i]);
+//         } 
+//         else {
+//             LGAOptions.append('option')
+//             .text(data[i]);
+//         }
+//     }
+// });
+
+// d3.select("#Sectors").selectAll("*").remove();
+// d3.json("getSectors").then(function(data) {
+//     SectorOptions = d3.select("#Sectors");
+//     SectorOptions.append('option').text('All');
+//     for (i = 0; i< data.length; i++) {
+//         if (Sector == "") {
+//             console.log(data[i])
+//             Sector = data[i];
+//             SectorOptions.append('option').attr('selected', '').text(data[i]);
+//         } 
+//         else if (Sector == data[i]) {
+//             SectorOptions.append('option').attr('selected', '').text(data[i]);
+//         } 
+//         else {
+//             SectorOptions.append('option').text(data[i]);
+//         }
+//     }
+// });
+
+// var bar = d3.select("#new-chart1")
+// .append('svg')
+// .attr('id', 'svg1')
+// .attr('height', barHeight - barMargin.top - barMargin.bottom)
+// .attr('width', barWidth - barMargin.left - barMargin.right)
+// .attr('viewBox', [0, 0, barWidth, barHeight])
+// .append('g')
+// .attr('id', 'svg1g1')
+// .attr('fill', "blue")
+// .attr("transform", "translate(" + barMargin.left + ", " + barMargin.top + ")");
+
+
+// var bar = d3.select("#new-chart2")
+// .append('svg')
+// .attr('id', 'svg2')
+// .attr('height', barHeight - barMargin.top - barMargin.bottom)
+// .attr('width', barWidth - barMargin.left - barMargin.right)
+// .attr('viewBox', [0, 0, barWidth, barHeight])
+// .append('g')
+// .attr('id', 'svg2g1')
+// .attr('fill', "blue")
+// .attr("transform", "translate(" + barMargin.left + ", " + barMargin.top + ")");
+
+
+// var bar = d3.select("#new-chart3")
+// .append('svg')
+// .attr('id', 'svg3')
+// .attr('height', barHeight - barMargin.top - barMargin.bottom)
+// .attr('width', barWidth - barMargin.left - barMargin.right)
+// .attr('viewBox', [0, 0, barWidth, barHeight])
+// .append('g')
+// .attr('id', 'svg3g1')
+// .attr('fill', "blue")
+// .attr("transform", "translate(" + barMargin.left + ", " + barMargin.top + ")");
+
+// Bottom lines are from Kristy's code that doesn't merge with ours currently, still working on connecting this.
